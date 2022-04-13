5d60c652825c760c78483a6ede122ad800896621
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2022-04-13 15:41:18 +0300

    pixel perfect, js для блока about

--- source/js/modules/modals/about.js
+++ source/js/modules/modals/about.js
@@ -1,14 +1,29 @@
 const aboutOptionalText = document.querySelector('.about__optional-text');
+const aboutOptionalTextMobile = document.querySelector('.about__optional-text-mobile');
 const aboutButton = document.querySelector('.about__button');
 
 const aboutButtonHandler = () => {
+
+  if (window.screen.width < 768) {
+    aboutOptionalTextMobile.classList.add('about__optional-text-mobile--hidden');
+  }
+
   document.addEventListener('click', (evt) => {
     if (evt.target.closest('.about__button')) {
       if (document.querySelector('.about__optional-text--hidden')) {
         aboutOptionalText.classList.remove('about__optional-text--hidden');
+
+        if (window.screen.width < 768) {
+          aboutOptionalTextMobile.classList.remove('about__optional-text-mobile--hidden');
+        }
+
         aboutButton.textContent = 'Свернуть';
       } else {
         aboutOptionalText.classList.add('about__optional-text--hidden');
+
+        if (window.screen.width < 768) {
+          aboutOptionalTextMobile.classList.add('about__optional-text-mobile--hidden');
+        }
         aboutButton.textContent = 'Подробнее';
       }
     }
