bbee8fcb7381ea1e6d24c8310f68992fa13af637
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2022-04-18 12:59:39 +0300

    правки по базовым критериям

--- source/js/modules/modals/about.js
+++ source/js/modules/modals/about.js
@@ -1,8 +1,11 @@
 const aboutOptionalText = document.querySelector('.about__optional-text');
 const aboutOptionalTextMobile = document.querySelector('.about__optional-text-mobile');
 const aboutButton = document.querySelector('.about__button');
+const aboutInformationWrapper = document.querySelector('.about__information-wrapper');
 
 const aboutButtonHandler = () => {
+
+  aboutInformationWrapper.classList.remove('about__information-wrapper--nojs');
 
   if (window.screen.width < 768) {
     aboutOptionalTextMobile.classList.add('about__optional-text-mobile--hidden');
